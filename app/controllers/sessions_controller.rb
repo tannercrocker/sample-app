21ffe603f215ac4cc71e9c6fa3ba874fbc08c344
Tanner Crocker <tec@email.uscupstate.edu>
2017-04-04 23:41:34 +0000

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = 'Welcome, ' + user[:name]
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
